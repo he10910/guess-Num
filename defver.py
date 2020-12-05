dd12b15751170dce1e610c447bb4581084924a7f
he10910 <>
2020-12-05 17:50:20 +0800

    新增猜的次數

--- defver.py
+++ defver.py
@@ -1,6 +1,10 @@
 import random
 import json
-from os import _exit as exit
+from os import _exit
+
+def exit():
+    input("輸入任意鍵繼續...")
+    _exit(0)
 
 with open("setting.json" , "r" , encoding="utf-8") as jfile:
     jdata = json.load(jfile)
@@ -15,41 +19,58 @@ def isint(str):                  #判斷輸入是否為整數
 error = jdata["Error_MSG"]
 maxx = jdata["Max"]
 minn = jdata["Min"]
+chance = jdata["Chances"]
 
 if isint(maxx) == True:         #判斷最大值是否正確
     maxx = int(maxx)
 else:
     print("錯誤的最大值")
-    exit(0)
+    exit()
 
 if isint(minn) == True:      #判斷最小值是否正確
     minn = int(minn)
 else:
     print("錯誤的最小值")
-    exit(0)
+    exit()
 
 if maxx < minn:            #判斷最小值是否大於最大值
     print("最小值已經大於最大值了")
-    exit(0)
+    exit()
+
 elif maxx - minn == 1:       #猜的範圍不包括最大及最小值本身
     print("沒有可以猜的範圍")
-    exit(0)
+    exit()
+
+if isint(chance) == True:      #判斷最小值是否正確
+    chance = int(chance)
+    if chance <= 0:
+        print("你沒有猜的次數")
+        exit()
+else:
+    print("錯誤的次數")
+    exit()
 
 ans = int(random.randint(int(minn+1),int(maxx-1)))         #設定答案
 
 gue = "x"
 while gue == "x":
-    gue = input("range is {} to {}:".format(minn,maxx))
+    if chance == 0:
+        print("次數已耗盡")
+        print("答案是",ans)
+        break
+    gue = input("範圍在{}到{}之間,你還有{}次機會:".format(minn,maxx,chance))
     if isint(gue) == True :
         gue = int(gue)
         if gue < ans and gue > minn:
             print(jdata["Too_Small_MSG"])
             minn = gue
             gue = "x"
+            chance -= 1
         elif gue > ans and gue < maxx:
             print(jdata["Too_Big_MSG"])
             maxx = gue
             gue = "x"
+            chance -= 1
         elif gue not in range(minn+1,maxx):
             print(jdata["Out_Of_Range_MSG"])
             gue = "x"
--- main.py
+++ main.py
@@ -12,6 +12,16 @@ def isint(str):                  #判斷輸入是否為整數
 minmax = "x"
 while minmax == "x":
 
+    minn = -1                  #定義最小值並判斷輸入是否為整數
+    while minn == -1:
+        minn = input("min:")
+        if isint(minn) == False:
+            print(error)
+            minn = -1
+        else:
+            minn = int(minn)
+            break
+
     maxx = -1                    #定義最大值並判斷輸入是否為整數
     while maxx == -1:
         maxx = input("max:")
@@ -22,16 +32,6 @@ while minmax == "x":
             maxx = int(maxx)
             break
 
-    minn = -1                  #定義最小值並判斷輸入是否為整數
-    while minn == -1:
-        minn = input("min:")
-        if isint(minn) == False:
-            print(error)
-            minn = -1
-        else:
-            minn = int(minn)
-            break
-    
     if maxx < minn:            #判斷最小值是否大於最大值
         print(error)
         minmax = "x"
@@ -40,21 +40,39 @@ while minmax == "x":
     else:
         minmax = "o"
 
+chance = "x"                 #定義次數並判斷輸入是否為整數
+while chance == "x":
+    chance = input("chances:")
+    if isint(chance) == False:
+        print(error)
+        chance = "x"
+    elif int(chance) <= 0:
+        print(error)
+        chance = "x"
+    else:
+        chance = int(chance)
+
 ans = int(random.randint(int(minn+1),int(maxx-1)))         #設定答案
 
 gue = "x"
 while gue == "x":
-    gue = input("range is {} to {}:".format(minn,maxx))
+    if chance == 0:
+        print("You have no chances.")
+        print("The answer is",ans)
+        break
+    gue = input("range is {} to {},and you have {} chances:".format(minn,maxx,chance))
     if isint(gue) == True :
         gue = int(gue)
         if gue < ans and gue > minn:
             print("too small")
             minn = gue
             gue = "x"
+            chance -=1
         elif gue > ans and gue < maxx:
             print("too big")
             maxx = gue
             gue = "x"
+            chance -= 1
         elif gue not in range(minn+1,maxx):
             print("out of range,try again!")
             gue = "x"
